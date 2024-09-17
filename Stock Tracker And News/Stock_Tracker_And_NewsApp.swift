54da044c72a76bd62e983be3078e0191de2dd69b
Sushant Ubale <sushantubale@gmail.com>
2024-09-17 11:29:44 -0500

    Initial Commit

---
+++ Stock Tracker And News/Stock_Tracker_And_NewsApp.swift
@@ -0,0 +1,20 @@
+//
+//  Stock_Tracker_And_NewsApp.swift
+//  Stock Tracker And News
+//
+//  Created by Sushant Ubale on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Stock_Tracker_And_NewsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
